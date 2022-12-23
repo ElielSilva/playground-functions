040f708be99a703aa11f0d6b30b13c66d2edba70
eliel <silvaeliel940@gmail.com>
2022-12-23 14:01:34 -0300

    fixed

--- src/challenges.js
+++ src/challenges.js
@@ -1,176 +1,145 @@
 // Desafio 1
-function compareTrue(valor1,valor2) {
-  return valor1 && valor2
+function compareTrue(valor1, valor2) {
+  return valor1 && valor2;
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  return (base*height)/2
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(palavra) {
-  return palavra.split(" ")
+  return palavra.split(' ');
 }
 
 // Desafio 4
 function concatName(lista) {
-  return lista[lista.length-1]+', '+lista[0]
+  return `${lista[lista.length - 1]}, ${lista[0]}`;
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
   // seu código aqui
-  return wins*3+ties*1
+  return wins * 3 + ties * 1;
 }
 
 // Desafio 6
 function highestCount(lista) {
-  // seu código aqui
   let retorno = 0;
-  let maior =-100;
+  let maior = -100;
   for (let contador of lista) {
     if (contador > maior) {
-      maior = contador
-    }
-
-  }
-  for (let repeticao of lista)  {
-    if (repeticao == maior) {
-      retorno+=1
+      maior = contador;
     }
   }
-  return retorno
+  for (let repeticao of lista) {
+    if (repeticao === maior) {
+      retorno += 1;
+    }
+  }
+  return retorno;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  // seu código aqui
   let poisition1 = 0;
   let poisition2 = 0;
   if (mouse < cat1) {
-    poisition1 = cat1 - mouse
-  }
-
-  else if (mouse > cat1)  {
-    poisition1 =mouse - cat1 
+    poisition1 = cat1 - mouse;
+  } else if (mouse > cat1) {
+    poisition1 = mouse - cat1;
   }
   if (mouse < cat2) {
-    poisition2 = cat2 - mouse
+    poisition2 = cat2 - mouse;
+  } else if (mouse > cat2) {
+    poisition2 = mouse - cat2;
   }
-
-  else if (mouse > cat2)  {
-    poisition2 =mouse - cat2 
-  }
-  
 
   if (poisition1 > poisition2) {
-    return "cat2"
+    return 'cat2';
   }
-  else if (poisition2 > poisition1) {
-    return 'cat1'
+  if (poisition2 > poisition1) {
+    return 'cat1';
   }
-  else if (poisition1 == poisition2) {
-    return  "os gatos trombam e o rato foge"
+  if (poisition1 == poisition2) {
+    return 'os gatos trombam e o rato foge';
   }
 }
 
 // Desafio 8
 function fizzBuzz(lista) {
-  let result =[];
+  let result = [];
   for (let number of lista) {
-    if (number%3==0 && number %5==0) {
-      result.push("fizzBuzz")
-    }
-    else if (number%3==0) {
-      result.push("fizz")
-    }
-    else if (number %5==0) {
-      result.push("buzz")
-    }
-    else {
-      result.push("bug!")
+    if (number % 3 == 0 && number % 5 == 0) {
+      result.push('fizzBuzz');
+    } else if (number % 3 == 0) {
+      result.push('fizz');
+    } else if (number % 5 == 0) {
+      result.push('buzz');
+    } else {
+      result.push('bug!');
     }
   }
-  return result
+  return result;
 }
 
 // Desafio 9
 function encode(code) {
-  // seu código aqui
-  // let code ="hello";
-  let palavra ='';
+  let palavra = '';
   for (let letra of code) {
     if (letra == 'a') {
-      palavra = palavra+'1'
+      palavra += '1';
+    } else if (letra == 'e') {
+      palavra += '2';
+    } else if (letra == 'i') {
+      palavra += '3';
+    } else if (letra == 'o') {
+      palavra = palavra += '4';
+    } else if (letra == 'u') {
+      palavra += '5';
+    } else if (letra == ' ') {
+      palavra += ' ';
+    } else {
+      palavra += letra;
     }
-    else if (letra == 'e') {
-      palavra = palavra+'2'
-    }
-    else if (letra == 'i') {
-      palavra = palavra+'3'
-    }
-    else if (letra == 'o') {
-      palavra = palavra= palavra+'4'
-      
-    }
-    else if (letra == 'u') {
-      palavra = palavra+'5'
-    }
-    else if (letra == ' ') {
-      palavra = palavra+' '
-    }
-    else {
-      palavra = palavra+letra
-    };
   }
-  return palavra
+  return palavra;
 }
 function decode(code) {
-  // seu código aqui
-  let palavra ='';
+  let palavra = '';
   for (let letra of code) {
     if (letra == '1') {
-      palavra = palavra+'a'
+      palavra += 'a';
+    } else if (letra == '2') {
+      palavra += 'e';
+    } else if (letra == '3') {
+      palavra += 'i';
+    } else if (letra == '4') {
+      palavra = palavra += 'o';
+    } else if (letra == '5') {
+      palavra += 'u';
+    } else if (letra == ' ') {
+      palavra += ' ';
+    } else {
+      palavra += letra;
     }
-    else if (letra == '2') {
-      palavra = palavra+'e'
-    }
-    else if (letra == '3') {
-      palavra = palavra+'i'
-    }
-    else if (letra == '4') {
-      palavra = palavra= palavra+'o'
-      
-    }
-    else if (letra == '5') {
-      palavra = palavra+'u'
-    }
-    else if (letra == ' ') {
-      palavra = palavra+' '
-    }
-    else {
-      palavra = palavra+letra
-    };
   }
-  return palavra
+  return palavra;
 }
 // Desafio 10
-function techList(lista,name) {
+function techList(lista, name) {
   // seu código aqui
-  if (lista.length == 0) {
-    return 'Vazio!'
+  if (lista.length === 0) {
+    return 'Vazio!';
   }
-  else {
-    let Array = [];
-    for (let novaTech of lista.sort()) {
-      Array.push({tech: novaTech,name: name})
-    }
-    return Array
+
+  let Array = [];
+  for (let novaTech of lista.sort()) {
+    Array.push({ tech: novaTech, name });
   }
+  return Array;
 }
-
-
 
 module.exports = {
   calcArea,
@@ -185,4 +154,3 @@ module.exports = {
   splitSentence,
   techList,
 };
-
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,79 +1,73 @@
 // Desafio 11
 function generatePhoneNumber(lista) {
-  // seu código aqui
-  
   if (lista.length != 11) {
-    return  "Array com tamanho incorreto."
+    return 'Array com tamanho incorreto.';
   }
   for (let zero of lista) {
     if (zero < 0 || zero > 9) {
-      return  "não é possível gerar um número de telefone com esses valores"
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
-  
+
   let valor = true;
   let contador = 0;
-  for (let x of lista)  {
-    for (let y of lista)  {
-      if (x===y) {
-        contador+=1; 
+  for (let x of lista) {
+    for (let y of lista) {
+      if (x === y) {
+        contador += 1;
       }
     }
     if (contador >= 3) {
-      return  'não é possível gerar um número de telefone com esses valores';
+      return 'não é possível gerar um número de telefone com esses valores';
     }
-    contador=0
+    contador = 0;
   }
-
-  let ddd = "";
-  let primeiro = "";
-  let segundo = "";
-  for (let index = 0; index <2 ; index+=1) {
-    ddd = ddd+lista[index]
+  let ddd = '';
+  let primeiro = '';
+  let segundo = '';
+  for (let index = 0; index < 2; index += 1) {
+    ddd += lista[index];
   }
-  for (let index = 2; index <7 ; index+=1) {
-    primeiro = primeiro+lista[index]
+  for (let index = 2; index < 7; index += 1) {
+    primeiro += lista[index];
   }
-  for (let index = 7; index <11 ; index+=1) {
-    segundo = segundo+lista[index]
+  for (let index = 7; index < 11; index += 1) {
+    segundo += lista[index];
   }
-  return '('+ ddd +') '+primeiro+'-'+segundo
+  return `(${ddd}) ${primeiro}-${segundo}`;
 }
-
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-  let a = Math.abs(lineA)
-  let b = Math.abs(lineB)
-  let c = Math.abs(lineC)
-  if (a>=b+c) {
-    return false
+  let a = Math.abs(lineA);
+  let b = Math.abs(lineB);
+  let c = Math.abs(lineC);
+  if (a >= b + c) {
+    return false;
   }
-  else if (b>=a+c) {
-    return false
+  if (b >= a + c) {
+    return false;
   }
-  else if (c>=b+a) {
-    return false
-  }else {
-    return true
+  if (c >= b + a) {
+    return false;
   }
+  return true;
 }
 
 // Desafio 13
 function hydrate(string) {
-  // seu código aqui
-  let numsStr = string.replace(/[^0-9]/g,'');
-  let lista = numsStr.split('')
+  let numsStr = string.replace(/[^0-9]/g, '');
+  let lista = numsStr.split('');
   console.log(lista);
-  let contador=0;
+  let contador = 0;
   for (let iterator of lista) {
-    contador = contador+Math.abs(iterator)
+    contador += Math.abs(iterator);
   }
   if (contador == 1) {
-    return  contador.toString()+' copo de água'
+    return `${contador.toString()} copo de água`;
   }
-  return contador.toString()+' copos de água'
+  return `${contador.toString()} copos de água`;
 }
 
 module.exports = {
